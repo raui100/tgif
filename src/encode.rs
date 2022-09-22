3cdca41bb48b3fff88363a189843b452836c779f
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2022-09-22 22:24:41 +0200

    Removed encoding with btree

--- src/encode.rs
+++ src/encode.rs
@@ -5,7 +5,6 @@ use ndarray::Axis;
 use nshare::ToNdarray2;
 
 use crate::args::Args;
-use crate::codebooks::u8_to_vec_bool;
 
 /// Encodes an image to TGIF
 pub fn encode(args: &Args) {
@@ -85,7 +84,6 @@ pub fn encode(args: &Args) {
 pub fn rice_index(delta: u8) -> u8 { RICE_INDEX[delta as usize] }
 
 const RICE_INDEX: [u8; 256] = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 255, 253, 251, 249, 247, 245, 243, 241, 239, 237, 235, 233, 231, 229, 227, 225, 223, 221, 219, 217, 215, 213, 211, 209, 207, 205, 203, 201, 199, 197, 195, 193, 191, 189, 187, 185, 183, 181, 179, 177, 175, 173, 171, 169, 167, 165, 163, 161, 159, 157, 155, 153, 151, 149, 147, 145, 143, 141, 139, 137, 135, 133, 131, 129, 127, 125, 123, 121, 119, 117, 115, 113, 111, 109, 107, 105, 103, 101, 99, 97, 95, 93, 91, 89, 87, 85, 83, 81, 79, 77, 75, 73, 71, 69, 67, 65, 63, 61, 59, 57, 55, 53, 51, 49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1];
-
 #[test]
 fn test_rice_index() {
     assert_eq!(rice_index(0), 0);
@@ -116,7 +114,6 @@ fn rice_code(image: &ndarray::Array2<u8>, parallel_encoding_units: u32, remainde
     );
 
     // Iterating over the image
-    let codebook = u8_to_vec_bool(remainder_bits, remainder_range);
     debug!("Encoding the image as Vec<bool>");
     for rows in image.axis_chunks_iter(Axis(0), parallel_encoding_units as usize) {
         let mut prev_pixels = prev_pixels_reset.clone();
@@ -126,20 +123,21 @@ fn rice_code(image: &ndarray::Array2<u8>, parallel_encoding_units: u32, remainde
             let prev_pixel = &prev_pixels[pos_y];
             let pixel = rows[(pos_y, pos_x)];
             let delta = prev_pixel.wrapping_sub(pixel);
-            // let index = rice_index(delta);
-            // let unary = index >> remainder_bits;
-            // let remainder = index & (two_pow_remainder_bits - 1);
+            let index = rice_index(delta);
+            let unary = index >> remainder_bits;
+            let remainder = index & (remainder_range - 1);
             prev_pixels[pos_y] = pixel;
 
-            let bools = codebook.get(&delta).unwrap();
-            img_bool.extend(bools);
+            // Unary coding. Pushes n "1" and one "0" to the vec
+            img_bool.extend(vec![true; unary as usize]);
+            img_bool.push(false);
 
-            // // Unary coding. Pushes n "1" and one "0" to the vec
-            // img_bool.extend(vec![true; unary as usize]);
-            // img_bool.push(false);
-            //
             // // Pushing the remainder to the vec
-            // img_bool.extend(remainder_to_vec_bool(remainder, remainder_bits));
+            img_bool.extend(
+                (0..remainder_bits)
+                .rev() // <-> Most significant bit
+                .map(|ind| remainder & POW_OF_TWO[ind as usize] != 0)
+            );
         }
     }
 
